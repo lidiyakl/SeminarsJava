218a962bcdd693acedf85501fc267c20efae91fa
«Lidiya» <lidiyakl0407@gmail.com>
2023-03-26 23:46:32 +0700

    hwTask3

--- src/main/java/homework1/homeWork1.java
+++ src/main/java/homework1/homeWork1.java
@@ -1,12 +1,16 @@
 package homework1;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class homeWork1 {
     public static void main(String[] args) {
         //printMinAndMax(10);
-        changeArr();
+        //changeArr();
+        inputNameAndPrint();
     }
 
     // 1. Задать одномерный массив и найти в нем минимальный и
@@ -57,4 +61,28 @@ public class homeWork1 {
         }
         System.out.println(Arrays.toString(nums));
     }
+
+//3*. В консоли запросить имя пользователя. В зависимости от текущего времени,
+//    вывести приветствие вида:
+//        "Доброе утро, <Имя>!", если время от 05:00 до 11:59
+//        "Добрый день, <Имя>!", если время от 12:00 до 17:59;
+//        "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+//        "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+
+    private static void inputNameAndPrint() {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+        if ((5 <= hour) && (hour < 12)){
+            System.out.printf("Доброе утро, %s!", name);
+        } else if ((12 <= hour) && (hour < 18)){
+            System.out.printf("Добрый день, %s!", name);
+        } else if ((18 <= hour) && (hour < 23)){
+            System.out.printf("Добрый вечер, %s!", name);
+        } else if ((23 == hour) | (hour < 5)){
+            System.out.printf("Доброй ночи, %s!", name);
+        }
+    }
 }
+
