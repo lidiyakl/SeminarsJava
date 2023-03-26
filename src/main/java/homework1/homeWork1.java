8795763d558a3815fd6b42fe200e5d3a99387268
«Lidiya» <lidiyakl0407@gmail.com>
2023-03-26 22:33:50 +0700

    hwTask2

--- src/main/java/homework1/homeWork1.java
+++ src/main/java/homework1/homeWork1.java
@@ -5,7 +5,8 @@ import java.util.Random;
 
 public class homeWork1 {
     public static void main(String[] args) {
-        printMinAndMax(10);
+        //printMinAndMax(10);
+        changeArr();
     }
 
     // 1. Задать одномерный массив и найти в нем минимальный и
@@ -28,5 +29,32 @@ public class homeWork1 {
             }
         }
         System.out.printf("Минимальный элемент равен %s. Максимальный элемент равен %s", min, max);
+    }
+
+//    2. Дан массив nums = [3,2,2,3] и число val = 3.
+//    Если в массиве есть числа, равные заданному, нужно перенести эти элементы
+//    в конец массива.
+//    Таким образом, первые несколько (или все) элементов массива должны быть отличны
+//    от заданного, а остальные - равны ему.
+
+    private static void changeArr() {
+        int[] nums = new int[]{3, 2, 2, 3};
+        int val = 3;
+        int size = nums.length;
+        int left = 0;
+        int right = size - 1;
+        int arr = 0;
+        while (left <= size/2) {
+            if (nums[left] == val) {
+                if (nums[right] == val) {
+                    right = right - 1;
+                }
+                arr = nums[left];
+                nums[left] = nums[right];
+                nums[right] = arr;
+            }
+            left++;
+        }
+        System.out.println(Arrays.toString(nums));
     }
 }
