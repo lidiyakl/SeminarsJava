db84ed9d9daec0b9ba3fd858e6d9207065e4df57
«Lidiya» <lidiyakl0407@gmail.com>
2023-03-30 22:34:49 +0700

    task2_2

--- src/main/java/lesson2/MainClassFromLessonTwo.java
+++ src/main/java/lesson2/MainClassFromLessonTwo.java
@@ -2,8 +2,10 @@ package lesson2;
 
 public class MainClassFromLessonTwo {
     public static void main(String[] args) {
-        task1();
+        //task1();
+        task2();
     }
+
 
     //    Дано число N (>0) и два символа с1 и с2.
 //    Нужно вывести на экран строку из символов с1 и с2,
@@ -23,7 +25,27 @@ public class MainClassFromLessonTwo {
         System.out.println(a);
     }
 //Напишите метод, который сжимает строку.
-//Пример: вход aaaabbbcdd. Выход: 4a3b1c2d
+//Пример: вход aaaabbbcdd. Выход: a4b3cd2
 
-
+    private static void task2() {
+        String a = "aaaabbbcdd";
+        StringBuilder res = new StringBuilder();
+        int count = 1;
+        for (int i = 0; i < a.length() - 1; i++) {
+            if (a.charAt(i) != a.charAt(i + 1)) {
+                res.append(a.charAt(i));
+                if (count > 1) {
+                    res.append(count);
+                }
+                count = 0;
+            }
+            count++;
+        }
+        res.append(a.charAt(a.length() - 1));
+        if (count > 1) {       //или res.append(count > 1 ? count : "");
+            res.append(count); // т.е. если count >1, то добавляем count,
+        }                      // иначе ничего не добавляем (пустая строка)
+        System.out.println(res);
+    }
 }
+
